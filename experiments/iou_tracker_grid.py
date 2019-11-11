766e009841a3ae2cd9a11c98d9c028b4ff2c87fc
Juan Navarro <jsnavarroa@unal.edu.co>
2019-11-10 19:22:21 -0500

    Save mot metrics to file.

--- experiments/iou_tracker_grid.py
+++ experiments/iou_tracker_grid.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 import motmetrics
 import numpy as np
+import pandas as pd
 
 from vvc import config
 from vvc.detector import object_detection
@@ -13,21 +14,30 @@ from vvc.vvc import VVC
 logging.basicConfig(format='%(asctime)s  %(message)s', level=logging.INFO)
 
 if __name__ == '__main__':
+
+    t_name = 'IOUTracker'
     videos = ['MOV_0861.mp4']
+    results = pd.DataFrame()
 
-    for video_name in videos:
-        mot_challenge_folder = Path(config.video_folder).joinpath('mot_challenge')
-        ground_true_file = mot_challenge_folder.joinpath(Path(video_name).stem + '.txt')
-        assert ground_true_file.exists()
+    detector_name = object_detection.Detector.RETINANET
+    detector = object_detection.get_detector(detector_name)
 
-        for model in [object_detection.Detector.TINY_YOLO3]:
-            detector = object_detection.get_detector(model)
+    mh = motmetrics.metrics.create()
+
+    for iou_threshold in np.arange(0.3, 0.8, 0.1):
+        for dectection_threshold in np.arange(0.5, 0.9, 0.1):
             accs = []
-            t_names = []
-            for iou_threshold in np.arange(0.3, 0.8, 0.1):
-                tracker = IOUTracker(iou_threshold=iou_threshold)
-                t_names.append('IOUTracker {}iou'.format(iou_threshold))
-                vvc = VVC(detector, tracker)
+            v_names = []
+            logging.info('Tracker params: iou_threshold={}, dectection_threshold={}'.format(iou_threshold,
+                                                                                            dectection_threshold))
+            tracker = IOUTracker(iou_threshold=iou_threshold, dectection_threshold=dectection_threshold)
+            vvc = VVC(detector, tracker)
+
+            for video_name in videos:
+                mot_challenge_folder = Path(config.video_folder).joinpath('mot_challenge')
+                ground_true_file = mot_challenge_folder.joinpath(Path(video_name).stem + '.txt')
+                assert ground_true_file.exists()
+
                 vvc_file = vvc.count(video_name, frame_rate_factor=0.5)
 
                 # To mot challenge
@@ -37,27 +47,26 @@ if __name__ == '__main__':
                 # Calc mot metrics
                 df_gt = motmetrics.io.loadtxt(ground_true_file)
                 df_test = motmetrics.io.loadtxt(mot_file)
-                accs.append(motmetrics.utils.compare_to_groundtruth(df_gt, df_test, 'iou', distth=0.5))
+                acc = motmetrics.utils.compare_to_groundtruth(df_gt, df_test, 'iou', distth=0.5)
+                if acc.events['HId'].notnull().sum() > 0:
+                    accs.append(acc)
+                    v_names.append(video_name)
 
-            metrics = [
-                'idf1',
-                'idp',
-                'idr',
-                'recall',
-                'precision',
-                'num_false_positives',
-                'num_misses',
-                'num_switches',
-                'num_fragmentations',
-                'mota',
-                'motp'
-            ]
+            # Summary of mot metrics for run
+            summary = mh.compute_many(accs,
+                                      metrics=motmetrics.metrics.motchallenge_metrics,
+                                      names=v_names,
+                                      generate_overall=True)
+            logging.info('\n' + motmetrics.io.render_summary(summary,
+                                                             namemap=motmetrics.io.motchallenge_metric_names,
+                                                             formatters=mh.formatters))
+            # Save results
+            summary['video'] = summary.index
+            summary['detector'] = detector_name
+            summary['tracker'] = t_name
+            summary['iou_threshold'] = iou_threshold
+            summary['dectection_threshold'] = dectection_threshold
+            results = results.append(summary.rename(columns=motmetrics.io.motchallenge_metric_names), ignore_index=True)
 
-            mh = motmetrics.metrics.create()
-            summary = mh.compute_many(accs, metrics=metrics, names=t_names, generate_overall=True)
-            print(motmetrics.io.render_summary(summary,
-                                               namemap=motmetrics.io.motchallenge_metric_names,
-                                               formatters=mh.formatters))
+    results.to_csv(Path(config.output_folder).joinpath('iou_tracker_grid.csv'), index=False, float_format='%.2f')
 
-
-            # Print best mot metrics and parameters
--- vvc/vvc.py
+++ vvc/vvc.py
@@ -32,6 +32,7 @@ def get_session():
 # use this environment flag to change which GPU to use
 #os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
+
 # set the modified tf session as backend in keras
 keras.backend.tensorflow_backend.set_session(get_session())
 
@@ -40,18 +41,18 @@ def miliseconds_from(last_time):
 	now = datetime.now()
 	diff = (now - last_time) / timedelta(microseconds=1) / 1000
 	return diff, now
-	
+
 
 class VVC(object):
-	
+
 	def __init__(self, detector, tracker=IOUTracker()):
 		self.model_name = detector.model_name
 		self.obj_detector = detector
 		self.tracker = tracker
-	
+
 	def format_img_yolo(self, img, img_min_side):
 		(height, width, _) = img.shape
-	
+
 		if width <= height:
 			f = img_min_side / width
 			new_height = int(f * height)
@@ -60,40 +61,43 @@ class VVC(object):
 			f = img_min_side / height
 			new_width = int(f * width)
 			new_height = int(img_min_side)
-		
+
 		img = cv2.resize(img, (new_width, new_height), interpolation=cv2.INTER_CUBIC)
-		
+
 		return img, f
-	
+
 	def accumulate(self, l):
 		it = itertools.groupby(l, operator.itemgetter(0))
 		for key, subiter in it:
 			yield key, sum(item[1] for item in subiter)
-	
+
 	def plot_box(self, img, box, color, label):
 		'''
 		Plot a box on an image
 		'''
 		img_box = img.copy()
 		(x1, y1, x2, y2) = box
-				
+
 		cv2.rectangle(img_box, (x1, y1), (x2, y2), color, 2)
 		text_label = label
-				
+
 		(retval, base_line) = cv2.getTextSize(text_label, cv2.FONT_HERSHEY_DUPLEX , 0.5, 1)
 		text_org = (x1, y1)
-	
+
 		base_line += 1
 		point1 = (text_org[0] - 5, text_org[1] + base_line - 5)
 		point2 = (text_org[0] + retval[0] + 5, text_org[1] - retval[1] - 5)
-	
+
 		cv2.rectangle(img_box, point1, point2, (0, 0, 0), 2)
 		cv2.rectangle(img_box, point1, point2, (255, 255, 255), -1)
 		cv2.putText(img_box, text_label, text_org, cv2.FONT_HERSHEY_DUPLEX , 0.5, (0, 0, 0), 1)
-		
+
 		return img_box
 
-	def count(self, video_name, frame_rate_factor=1, filter_tags=['bicycle', 'car', 'motorbike', 'bus', 'truck'], show_obj_id=True):
+	def count(self, video_name,
+			  frame_rate_factor=1,
+			  filter_tags=['bicycle', 'car', 'motorbike', 'bus', 'truck'],
+			  show_obj_id=True):
 		# Load video and parameters
 
 		logging.info('Load video and parameters')
@@ -118,109 +122,109 @@ class VVC(object):
 		data.timestamps['start'] = datetime.now().isoformat()
 		data.video.input_file = input_video_file
 		data.video.output_file = output_video_file
-									
+
 		reader = input_video.video_reader()
-		
+
 		video_writer = SKVideoWriter(output_video_file, frame_rate)
-	
+
 		frame_id = 0
 		total_frames = input_video.total_frames()
-		
+
 		class_mapping = self.obj_detector.get_class_mapping()
-			
+
 		class_to_color = {class_mapping[v]: np.random.randint(0, 255, 3).tolist() for v in class_mapping}
-		
+
 		data.timestamps['warm'] = datetime.now().isoformat()
-		
+
 		last_time = datetime.now()
-			
+
 		with tqdm(total=total_frames, unit='frame') as pbar:
-		
+
 			for frame in reader:
-				
+
 				frame_id += 1
 				img_name = str(frame_id) + ".jpg"
-				
+
 				tqdm_step = int(total_frames / 10)
 				if frame_id % tqdm_step == 0:
 					pbar.update(tqdm_step)
-				
+
 				# Save frame data
 				frame_data = data.add_frame_data()
 				frame_data.name = img_name
 				frame_data.timestamps['read'], last_time = miliseconds_from(last_time)
-				
+
 				# Image preprocesing
 				img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-				
+
 				# Resize input image
 				X, scale_factor = self.format_img_yolo(img, 600)
-		
+
 				img_scaled = X.astype(np.uint8)
-				
+
 				frame_data.timestamps['preprocessing'], last_time = miliseconds_from(last_time)
-				
+
 				# Objetct detection
-				
+
 				bboxes = self.obj_detector.predict(X)
-						
+
 				for bbox in bboxes:
-					
+
 					tag = bbox['class']
-					
+
 					# Save annotations
-					if tag in filter_tags: #and bbox['prob'] > 0.5:
+					if tag in filter_tags:
 						object_data = frame_data.add_object()
 						object_data.tag = tag
 						object_data.box = bbox['box']
 						object_data.probability = float(bbox['prob'])
-				
+
 				frame_data.timestamps['detection'], last_time = miliseconds_from(last_time)
-				
+
 				# Tracking the objects
 				tracked_objects = self.tracker.tracking(frame_data.objects)
-				
+
 				frame_data.timestamps['tracking'], last_time = miliseconds_from(last_time)
-								
+
 				# Plot tracking results
 				img_tracks = img_scaled.copy()
-				
+
 				for object_data in tracked_objects:
-					
+
 					box = object_data.boxes[-1]
 					color = class_to_color[object_data.tag]
 					label = '{}'.format(object_data.name)
-					
+
 					track_data = frame_data.add_track()
 					track_data.id = object_data.name
 					track_data.box = np.array(box) * (1/scale_factor)
-					
+
 					if not show_obj_id:
 						label = label.split(sep=' ')[0]
-						
+
 						label += ' {0:.0%}'.format(object_data.probability)
-										
+
 					img_tracks = self.plot_box(img_tracks, box, color, label)
-				
+
 				# Save final image
 				img_post = img_tracks
-				
+
 				img_post = cv2.cvtColor(img_post, cv2.COLOR_RGB2BGR)
-				
+
 				video_writer.writeFrame(img_post);
-							
+
 				frame_data.timestamps['postprocessing'], last_time = miliseconds_from(last_time)
 
 		data.timestamps['end_loop'] = datetime.now().isoformat()
-		
+
 		video_writer.close()
-		
-		data.timestamps['write'] = datetime.now().isoformat()	
-		
+
+		data.timestamps['write'] = datetime.now().isoformat()
+
 		# Save data to json file
 		json_file = output_video_file + ".json"
 		json_utils.save_to_json(data, json_file)
 
 		return json_file
-		
+
 
